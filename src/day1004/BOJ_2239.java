456c297a9ad3abfa3c7a4bc610f4a99af87a2c87
YangHyunYong <9935ok9938@gmail.com>
2022-10-05 09:46:00 +0900

    스도쿠 11등!

--- src/day1004/BOJ_2239.java
+++ src/day1004/BOJ_2239.java
@@ -3,17 +3,18 @@ package day1004;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class BOJ_2239 { // BitMasking
-	static int[][] maps = new int[9][9];	//맵
-	static int[] rowBM = new int[9];		//행 BM 리스트
-	static int[] colBM = new int[9];		//열 BM 리스트
-	static int[] blockBM = new int[9];		//블록 BM 리스트
+	static int[][] maps = new int[9][9]; // 맵
+	static int[] rowBM = new int[9]; // 행 BM 리스트
+	static int[] colBM = new int[9]; // 열 BM 리스트
+	static int[] blockBM = new int[9]; // 블록 BM 리스트
 	static boolean done = false;
-	static Queue<int[]> dirXY = new LinkedList<>();
-
+	static ArrayList<int[]> dirXY = new ArrayList<>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
 		for (int i = 0; i < 9; i++) {
@@ -49,10 +50,10 @@ public class BOJ_2239 { // BitMasking
 			}
 			blockBM[k - 1] = tempBM;
 		}
-
 		// BM 매핑 완료
-		while (!dirXY.isEmpty()) { // 채워야하는 곳들 하나씩 봤을 때
-			int[] temp = dirXY.poll();
+		int len = dirXY.size();
+		for (int tc = 0; tc < len; tc++) {// 채워야하는 곳들 하나씩 봤을 때
+			int[] temp = dirXY.remove(0);
 			int x = temp[0];
 			int y = temp[1];
 			int k = temp[2];
@@ -61,8 +62,7 @@ public class BOJ_2239 { // BitMasking
 			int bBM = blockBM[k];
 			int count = 0;
 			for (int i = 1; i <= 9; i++) {
-				if ((rBM & (1 << i)) == 0 && (cBM & (1 << i)) == 0 && (bBM & (1 << i)) == 0) { // 행에도 없고 열에도 없고 블록에도 없는
-																								// 숫자
+				if ((rBM & (1 << i)) == 0 && (cBM & (1 << i)) == 0 && (bBM & (1 << i)) == 0) { // 행에도 없고 열에도 없고 블록에도 없는 숫자
 					if (count != 0) { // 들어갈 수 있는 숫자가 하나 이상이면 일단 패스
 						count = -1;
 						break;
@@ -71,6 +71,7 @@ public class BOJ_2239 { // BitMasking
 				}
 			}
 			if (count == -1) {
+				dirXY.add(temp);
 				continue;
 			}
 			rowBM[x] |= 1 << count;
@@ -83,30 +84,27 @@ public class BOJ_2239 { // BitMasking
 	}
 
 	private static void DFS(int count) {
-		if (count == 81) {
-			done = true;	//완성했다!
+		if (count >= dirXY.size()) {
+			done = true; // 완성했다!
 			return;
 		}
-		int[] temp = getXY(count);
-		int x = temp[0];
-		int y = temp[1];
-		if (maps[x][y] != 0) {
-			DFS(count + 1);
-		} else {
-			for (int i = 1; i <= 9; i++) {
-				if ((rowBM[x] & (1 << i)) == 0 && (colBM[y] & (1 << i)) == 0 && (blockBM[getK(x, y)] & (1 << i)) == 0) {	//선택되지 않은 숫자야!
-					rowBM[x] |= 1 << i;
-					colBM[y] |= 1 << i;
-					blockBM[getK(x, y)] |= 1 << i;
-					maps[x][y] = i;
-					DFS(count + 1);
-					if (done)
-						return;
-					maps[x][y] = 0;	// i로 보내봤는데 다음 경로에서 해당 숫자를 또 써야해 => 재귀함수 복귀 및 i 다음 숫자로 재시도
-					rowBM[x] &= ~(1 << i);
-					colBM[y] &= ~(1 << i);
-					blockBM[getK(x, y)] &= ~(1 << i);
-				}
+		int [] point = dirXY.get(count);
+		int x = point[0];
+		int y = point[1];
+		int k = point[2];
+		for (int i = 1; i <= 9; i++) {
+			if ((rowBM[x] & (1 << i)) == 0 && (colBM[y] & (1 << i)) == 0 && (blockBM[k] & (1 << i)) == 0) { // 선택되지 않은  숫자야!
+				rowBM[x] |= 1 << i;
+				colBM[y] |= 1 << i;
+				blockBM[k] |= 1 << i;
+				maps[x][y] = i;
+				DFS(count+1);
+				if (done)
+					return;
+				maps[x][y] = 0;
+				rowBM[x] &= ~(1 << i);
+				colBM[y] &= ~(1 << i);
+				blockBM[k] &= ~(1 << i);
 			}
 		}
 	}
@@ -120,39 +118,7 @@ public class BOJ_2239 { // BitMasking
 		}
 	}
 
-	private static int[] getXY(int count) {
-		int x = count / 9;
-		int y = count % 9;
-		return new int[] { x, y };
-	}
-
 	private static int getK(int x, int y) { // 블록 번호 구하기
-		int k = 0;
-		if (x < 3) {
-			if (y < 3) {
-				k = 0;
-			} else if (y < 6) {
-				k = 1;
-			} else {
-				k = 2;
-			}
-		} else if (x < 6) {
-			if (y < 3) {
-				k = 3;
-			} else if (y < 6) {
-				k = 4;
-			} else {
-				k = 5;
-			}
-		} else {
-			if (y < 3) {
-				k = 6;
-			} else if (y < 6) {
-				k = 7;
-			} else {
-				k = 8;
-			}
-		}
-		return k;
+		return x/3*3 + y / 3;
 	}
 }
