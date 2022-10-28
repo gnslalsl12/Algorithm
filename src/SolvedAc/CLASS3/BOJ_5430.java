493b8ad3db1a54b658979e0077dbfa759f38c924
SSAFY <SSAFY@DESKTOP>
2022-10-28 10:46:25 +0900

    5430

--- src/SolvedAc/CLASS3/BOJ_5430.java
+++ src/SolvedAc/CLASS3/BOJ_5430.java
@@ -3,72 +3,61 @@ package SolvedAc.CLASS3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 
 public class BOJ_5430 {
-	static String orders;
-	static int N;
-	static Deque<Integer> sols;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader read= new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(read.readLine());
+		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		for(int test = 1; test <= T; test++) {
-			orders = read.readLine();
-			N = Integer.parseInt(read.readLine());
-			sols = new LinkedList<>();
-			String line = read.readLine();
-			for(int i = 0; i < line.length(); i++) {
-				if(line.charAt(i) == '[') {
-					line.replace("[", "");
-				}else if(line.charAt(i) == ']') {
-					System.out.println("?HJSKLHJKLA");
-					line.replace("]", "");
-				}
-			}
-			System.out.println(line.toString() + "??????");
-			String[] templine = line.split(",");
-			System.out.println("///////////////////");
-			System.out.println(Arrays.toString(templine));
-			if(N > 0) {
-				for(int i = 1; i < templine.length-1; i++) {
-					sols.add(Integer.parseInt(templine[i]));
-				}
-			}
-			boolean out = false;
-			boolean reverse = false;
-			System.out.println("sols : " + sols);
-			for(int i = 0; i < orders.length(); i++) {
-				if(orders.charAt(i) == 'R') {
-					reverse = !reverse;
+
+		int T = Integer.parseInt(read.readLine());
+		continuefirst: for (int test = 0; test < T; test++) {
+			StringBuilder tempsb = new StringBuilder();
+			String orders = read.readLine();
+			int N = Integer.parseInt(read.readLine());
+			String templine = read.readLine();
+			Deque<Integer> dq = new LinkedList<>();
+			for (int i = 1; i < templine.length() - 1; i++) {
+				if (templine.charAt(i) == ',')
 					continue;
+				dq.add(templine.charAt(i) - '0');
+			}
+			boolean reverse = false;
+			for (int i = 0; i < orders.length(); i++) {
+				if (orders.charAt(i) == 'R')
+					reverse = !reverse;
+				else {
+					if (dq.isEmpty()){
+						sb.append("error\n");
+						continue continuefirst;
+					}
+					if (reverse) {
+						dq.pollLast();
+					} else {
+						dq.pollFirst();
+					}
+				}
+			}
+			tempsb.append("[");
+			if(dq.isEmpty()) {
+				sb.append("error\n");
+				continue;
+			}
+			while (!dq.isEmpty()) {
+				if(reverse) {
+					tempsb.append(dq.pollLast());
 				}
 				else {
-					if(sols.isEmpty()) {
-						out = true;
-						sb.append("error\n");
-						break;
-					}
-					if(reverse) {
-						sols.pollLast();
-					}else {
-						sols.pollFirst();
-					}
+					tempsb.append(dq.poll());
+				}
+				if (!dq.isEmpty()) {
+					tempsb.append(",");
 				}
 			}
-			if(!out) {
-				sb.append("[");
-				for(int i = 0; i < sols.size(); i++) {
-					sb.append(sols.poll());
-					if(i != sols.size()-1) {
-						sb.append(",");
-					}
-				}
-				sb.append("]\n");
-			}
+			tempsb.append("]\n");
+			sb.append(tempsb);
 		}
 		System.out.print(sb);
 	}
