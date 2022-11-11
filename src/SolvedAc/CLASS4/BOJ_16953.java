696cb216dba373e79581e34c47f48b0c3f9c72f5
wjdgnsxhsl@naver.com <wjdgnsxhsl@naver.com>
2022-11-11 17:55:04 +0900

    gg

---
+++ src/SolvedAc/CLASS4/BOJ_16953.java
@@ -0,0 +1,56 @@
+package SolvedAc.CLASS4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BOJ_16953 {
+	static long A, B;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer tokens = new StringTokenizer(read.readLine());
+		A = Integer.parseInt(tokens.nextToken());
+		B = Integer.parseInt(tokens.nextToken());
+		PriorityQueue<hoons> pq = new PriorityQueue<>();
+		pq.add(new hoons(A, 1));
+		int result = Integer.MAX_VALUE;
+		while (!pq.isEmpty()) {
+			hoons temp = pq.poll();
+
+			if (temp.num * 2 == B || temp.num * 10 + 1 == B) {
+				result = Math.min(result, temp.count + 1);
+				break;
+			}
+
+			if (temp.num * 2 > B)
+				continue;
+			pq.add(new hoons(temp.num * 2, temp.count + 1));
+
+			if (temp.num * 10 + 1 > B)
+				continue;
+			pq.add(new hoons(temp.num * 10 + 1, temp.count + 1));
+		}
+		if (result == Integer.MAX_VALUE)
+			result = -1;
+		System.out.println(result);
+	}
+
+	private static class hoons implements Comparable<hoons> {
+		long num;
+		int count;
+
+		public hoons(long num, int count) {
+			super();
+			this.num = num;
+			this.count = count;
+		}
+
+		@Override
+		public int compareTo(hoons o) {
+			return this.count == o.count ? Long.compare(o.num, this.num) : Integer.compare(this.count, o.count);
+		}
+	}
+}
---
+++ src/day1111/BOJ_9935.java
@@ -0,0 +1,57 @@
+package day1111;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class BOJ_9935 {
+	static String tempread;
+	public static void main(String[] args) {
+		BufferedReader bread = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader btempread = new BufferedReader(new InputStreamReader(System.in));
+		String read= bread.toString();
+		tempread = btempread.toString();
+		while (true) {
+			boolean tempresult = check(read, tempread);
+			if(!tempresult) break;
+		}
+		System.out.println();
+
+	}
+
+	private static boolean check(String read, String tempread) {
+		String removed = "";
+		Queue<Integer> remindex = new LinkedList<Integer>();
+		breakall: for (int i = 0; i < read.length(); i++) {
+			if (read.charAt(i) == tempread.charAt(0)) {
+				for (int j = 1; j < tempread.length(); j++) {
+					if (i + j >= read.length())
+						break breakall;
+					if (read.charAt(i + j) != tempread.charAt(j)) {
+						break;
+					}
+					if (j == tempread.length()) {
+						remindex.add(i);
+					}
+				}
+			}
+		}
+		if (!remindex.isEmpty()) {
+			for (int i = 0; i < read.length(); i++) {
+				if (!remindex.isEmpty() && i == remindex.peek()) {
+					i = remindex.poll();
+				}
+				removed += read.charAt(i);
+			}
+			read = removed;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
