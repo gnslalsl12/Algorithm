afc39c5aca6b430bfc15b139cd940725bda9dbb3
gnslalsl12 <40541305+gnslalsl12@users.noreply.github.com>
2023-07-27 16:17:30 +0900

    [Gold V] Title: 감시 피하기, Time: 88 ms, Memory: 13168 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.java
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.java
@@ -1,107 +1,103 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+
 	static int N;
-	static char [][] maps;
-	static ArrayList<int []> Empty = new ArrayList<>();
-	static ArrayList<int []> Teachers = new ArrayList<>();
-	static int[][] deltas = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
-	static boolean Success;
+	static int[][] Map;
+	static ArrayList<int[]> TList;
+	static int[][] Deltas = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+	static String Result;
+
 	public static void main(String[] args) throws IOException {
+		BufferedWriter write = new BufferedWriter(new OutputStreamWriter(System.out));
+		init();
+		solv();
+		write.write(Result + "\n");
+		write.close();
+	}
+
+	private static void init() throws IOException {
 		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer tokens = new StringTokenizer(read.readLine());
-		N = Integer.parseInt(tokens.nextToken());
-		maps = new char [N][N];
-		for(int i = 0; i < N; i++) {
-			tokens = new StringTokenizer(read.readLine());
-			for(int j = 0; j < N; j++) {
-				maps[i][j] = tokens.nextToken().charAt(0);
-				if(maps[i][j] == 'X') {
-					Empty.add(new int [] {i,j});
-				}else if(maps[i][j] == 'T') {
-					Teachers.add(new int [] {i,j});
+		N = Integer.parseInt(read.readLine());
+		Map = new int[N][N];
+		TList = new ArrayList<>();
+		Result = "NO";
+		for (int i = 0; i < N; i++) {
+			StringTokenizer tokens = new StringTokenizer(read.readLine());
+			for (int j = 0; j < N; j++) {
+				char ij = tokens.nextToken().charAt(0);
+				if (ij == 'S') {
+					Map[i][j] = -1;
+				} else if (ij == 'T') {
+					Map[i][j] = -2;
+					TList.add(new int[] { i, j });
 				}
 			}
 		}
-		for(int [] tempT : Teachers) {
-			if(NexttoT(tempT)) {
-				System.out.println("NO");
-				return;
-			}
-		}
-		Success = false;
-		Comb(0, 0, (long)0);
-		
-		if(Success) System.out.println("YES");
-		else System.out.println("NO");
-		return;
-		
+		read.close();
 	}
-	
-	private static boolean NexttoT(int [] XY) {
-		for(int dir = 0; dir < 4; dir++) {
-			int tempx = XY[0] + deltas[dir][0];
-			int tempy = XY[1] + deltas[dir][1];
-			if(!isIn(tempx, tempy)) continue;
-			if(maps[tempx][tempy] == 'S') return true;
-		}
-		return false;
+
+	private static void solv() {
+		getCombOLocs();
 	}
-	
-	private static void Comb(int countnow, int startpoint, long SelBM) {
-		if(Success) return; //앞의 경우에서 성공함
-		if(countnow == 3) {
-			//맵마다 설치해서 선생이 지켜보기
-			char [][] exmaps = new char[N][N];
-			for(int i = 0; i < N; i++) {
-				exmaps[i] = maps[i].clone();
-			}
-			TeacherView(SelBM, exmaps);
+
+	private static void getCombOLocs() { // O 위치 조합
+		getComb(0, 0, new int[3]);
+	}
+
+	private static void getComb(int count, int start, int[] sel) {
+		if (Result == "YES")
+			return;
+		else if (count == 3) {
+			setObjAtLocs(sel);
 			return;
 		}
-		
-		for(int i = startpoint; i < Empty.size(); i++) {
-			SelBM |= 1<<i;
-			Comb(countnow + 1, i+1, SelBM);
-			SelBM &= ~(1<<i);
+
+		for (int current = start; current < N * N; current++) {
+			int i = current / N;
+			int j = current % N;
+			if (Map[i][j] < 0)
+				continue;
+			sel[count] = current;
+			getComb(count + 1, current + 1, sel);
 		}
 	}
-	
-	private static void TeacherView(long BM, char[][] tempmaps) {
-		int count = 0;
-		for(int index = 0; index < Empty.size() ; index++) {
-			if((BM & 1<<index) != 0) {	//선택된 index
-				tempmaps[Empty.get(index)[0]][Empty.get(index)[1]] = 'O';
-				count++;
-			}
-			if(count == 3) break;
+
+	private static void setObjAtLocs(int[] sel) { // 조합된 위치에 O 세팅하고 T 시선 처리
+		for (int loc : sel) {
+			int i = loc / N;
+			int j = loc % N;
+			Map[i][j] = 1;
 		}
-		for(int [] tempXY : Teachers) {
-			for(int dir = 0; dir < 4; dir++) {
-				for(int len = 1; len < N; len ++) {
-					int tempx = tempXY[0] + deltas[dir][0]*len;
-					int tempy = tempXY[1] + deltas[dir][1]*len;
-					if(!isIn(tempx, tempy)) break;
-					if(tempmaps[tempx][tempy] == 'O') {
-						break;
-					}
-					if(tempmaps[tempx][tempy] == 'S') {
-						tempmaps = maps.clone();
-						return;	//다른 경우의 수로 돌아가기
+		boolean founded = false;
+		breakAll: for (int[] tLoc : TList) {
+			for (int[] delta : Deltas) {
+				for (int len = 1; len < N * N; len++) {
+					int nextI = tLoc[0] + delta[0] * len;
+					int nextJ = tLoc[1] + delta[1] * len;
+					if (!isIn(nextI, nextJ) || Map[nextI][nextJ] == 1) // 범위 밖 / 장애물로 시선 차단됨
+						break; // 학생 발견 X
+					else if (Map[nextI][nextJ] == -1) { // 학생 발견
+						founded = true;
+						break breakAll;
 					}
 				}
 			}
 		}
-		//어디에서도 학생을 찾을 수 없음
-		Success = true;
-		return;
+		if (!founded)
+			Result = "YES";
+		else {
+			for (int loc : sel) {
+				int i = loc / N;
+				int j = loc % N;
+				Map[i][j] = 0;
+			}
+		}
 	}
-	
-	private static boolean isIn(int tempx, int tempy) {
-		return tempx >= 0 && tempx < N && tempy >= 0 && tempy < N;
+
+	private static boolean isIn(int i, int j) {
+		return i >= 0 && j >= 0 && i < N && j < N;
 	}
+
 }
