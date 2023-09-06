b5504a9abdb22fa5cc9e662ee703c8469d07ca8b
gnslalsl12 <40541305+gnslalsl12@users.noreply.github.com>
2023-09-06 14:56:53 +0900

    [Gold V] Title: 강의실 배정, Time: 396 ms, Memory: 25856 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -2,10 +2,9 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N;
+   static int N;
     static Comparator<int[]> Comp;
     static PriorityQueue<int[]> ClassList;
-    static int Result;
     
     public static void main(String[] args) throws IOException{
         init();
@@ -13,28 +12,21 @@ public class Main {
     }
     
     private static void init()throws IOException{
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(read.readLine());
+        N = readInt();
         Comp = new Comparator<int[]>(){
             @Override
             public int compare(int[] x1, int[] x2){
-                return x1[0]==x2[0]? Integer.compare(x1[1], x2[1]) : Integer.compare(x1[0],x2[0]);
+                return x1[0] - x2[0];   //끝시간은 후의 PQ에서 정렬할 거니까까
             }
         };
         ClassList = new PriorityQueue<>(Comp);
         while(N-- > 0){
-            StringTokenizer tokens = new StringTokenizer(read.readLine());
-            ClassList.add(new int [] {Integer.parseInt(tokens.nextToken()), Integer.parseInt(tokens.nextToken())});
+            ClassList.add(new int [] {readInt(), readInt()});
         }
-        Result = 0;
-        read.close();
     }
     
     private static void solv() throws IOException{
-        BufferedWriter write = new BufferedWriter(new OutputStreamWriter(System.out));
         getClassRoomCount();
-        write.write(Result + "\n");
-        write.close();
     }
     
     private static void getClassRoomCount(){
@@ -42,13 +34,18 @@ public class Main {
         endTimeList.add(0);
         //=> 가장 빨리 끝나는 하나의 강의실로도 커버가 안되면 뒤에 강의실도 다 안되는 거니까
         while(!ClassList.isEmpty()){
-            int earliestEndTime = endTimeList.peek();   //가장 빨리 끝나는 강의실 시간
             int [] nextClass = ClassList.poll();    //다음 강의의
-            if(earliestEndTime <= nextClass[0]){ //가장 빨리 끝나는 강의 다음으로 붙일 수 있음
+            if(endTimeList.peek() <= nextClass[0]){ //가장 빨리 끝나는 강의 다음으로 붙일 수 있음
                 endTimeList.poll(); //해당 강의실 시간 없애기
             }
             endTimeList.add(nextClass[1]);//새로운 강의 끝시간 추가
         }
-        Result = endTimeList.size();
+        System.out.println(endTimeList.size());
+    }
+    
+    private static int readInt()throws IOException{
+        int c, n = System.in.read() & 15;   //n은 숫자 한 자리
+        while((c = System.in.read()) > 32) n = (n<<3) + (n<<1) + (c&15);
+        return n;
     }
 }
