d95d39a6685c78e1e277dae23bd8b598b93f9966
gnslalsl12 <40541305+gnslalsl12@users.noreply.github.com>
2023-09-06 12:42:34 +0900

    [Gold V] Title: 평범한 배낭, Time: 112 ms, Memory: 12340 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -5,8 +5,8 @@ public class Main
 {
     static int N;
     static int K;
-    static int[][] ItemList;
-    static int [][] DPBag;
+    static Queue<int[]> ItemList;
+    static int [] DPBag;
     static long Result;
 
     public static void main(String[] args)throws IOException
@@ -20,12 +20,12 @@ public class Main
         StringTokenizer tokens = new StringTokenizer(read.readLine());
         N = Integer.parseInt(tokens.nextToken());
         K = Integer.parseInt(tokens.nextToken());
-        ItemList = new int [N][2];
+        ItemList = new LinkedList<>();
         for(int n = 0; n < N; n++){
             tokens = new StringTokenizer(read.readLine());
-            ItemList[n] = new int [] {Integer.parseInt(tokens.nextToken()), Integer.parseInt(tokens.nextToken())};
+            ItemList.add(new int [] {Integer.parseInt(tokens.nextToken()), Integer.parseInt(tokens.nextToken())});
         }
-        DPBag = new int [N+1][K+1];
+        DPBag = new int [K+1];
         Result = 0L;
         read.close();
     }
@@ -38,20 +38,14 @@ public class Main
     }
 
     private static void getDPBagStack(){
-        for(int n = 0; n < N; n++){    //n번 아이템까지 고려했을 때
-            for(int k= 1; k <= K; k++){ //들어갈 수 있는 무게를 k라고 한다면
-                if(n == 0 && k >= ItemList[0][0]){
-                    DPBag[n][k] = ItemList[0][1];
-                    continue;
-                }
-                if(k >= ItemList[n][0]){    //n 아이템을 넣을 수 있는 배낭 무게라면
-                    DPBag[n][k] = Math.max(DPBag[n>= 1? n-1 : 0][k], DPBag[n>= 1? n-1 : 0][k-ItemList[n][0]] + ItemList[n][1]);
-                }else{  //아니라면면
-                    DPBag[n][k] = DPBag[n>= 1? n-1 : 0][k];   
-                }
+        while(!ItemList.isEmpty()){
+            int [] item = ItemList.poll();
+            for(int k = K; k >= item[0]; k--){  //고려하는 배낭 무게가 item의 무게를 버틸 수 있는 경우만 탐색
+                if(DPBag[k] < DPBag[k-item[0]] + item[1]) DPBag[k] = DPBag[k-item[0]] + item[1];
+                //이전까지 본 모든 item을 고려한 경우들 중 이번 item을 넣을 수 있는 배낭 상태들 중에서 이번 item 가치를 더헀을 때가 더 가치가 높다면 그걸로 기록
             }
         }
-        Result = DPBag[N-1][K];
+        Result = DPBag[K];
     }
 
 }
