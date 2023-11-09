c60af437ff35689c76cf050ce3dd0886ee298c5b
gnslalsl12 <40541305+gnslalsl12@users.noreply.github.com>
2023-11-09 15:07:26 +0900

    [Gold IV] Title: 여행 가자, Time: 100 ms, Memory: 12152 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,44 +1,38 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-	static int N, M;
-	static boolean[][] maps;
+public class Main { // UnionFind
+	static int N;
+	static int M;
+	static int[] Parents;
+	static Queue<Integer> Plan;
 
-	public static void main(String[] args) throws IOException {
-		BufferedWriter write = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer tokens;
+	public static void main(String args[]) throws IOException {
+		init();
+		solv();
+	}
+
+	private static void init() throws IOException {
 		N = readInt();
 		M = readInt();
-		maps = new boolean[N][N];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (readInt() == 1) {
-					maps[i][j] = true;
-					maps[j][i] = true;
+		Parents = new int[N];
+		for (int n = 0; n < N; n++) {
+			Parents[n] = n;
+		}
+		for (int n = 0; n < N; n++) {
+			for (int nn = 0; nn < N; nn++) {
+				if (readInt() == 1 && !isSameParent(n, nn)) {
+					setParents(n, nn);
 				}
 			}
 		}
-		FLYD();
-		boolean answer = true;
-		int start = readInt() - 1;
-		int next;
-		for (int i = 1; i < M; i++) {
-			next = readInt() - 1;
-			if (!maps[start][next]) {
-				answer = false;
-				break;
-			}
-			start = next;
+		Plan = new LinkedList<>();
+		for (int m = 0; m < M; m++) {
+			Plan.add(readInt() - 1);
 		}
-		if (answer)
-			write.write("YES\n");
-		else
-			write.write("NO\n");
-		write.close();
 	}
-    
-    private static int readInt() throws IOException {
+
+	private static int readInt() throws IOException {
 		int n, c;
 		boolean neg = false;
 		do {
@@ -53,17 +47,41 @@ public class Main {
 		return neg ? -n : n;
 	}
 
-	private static void FLYD() {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				for (int k = 0; k < N; k++) {
-					if (i == j || maps[i][k] && maps[k][j]) {
-						maps[i][j] = true;
-						maps[j][i] = true;
-					}
-				}
-			}
+	private static boolean isSameParent(int child1, int child2) {
+		return getParent(child1) == getParent(child2);
+	}
+
+	private static void setParents(int child1, int child2) {
+		int parent1 = getParent(child1);
+		int parent2 = getParent(child2);
+		if (parent1 <= parent2) {
+			Parents[parent2] = parent1;
+		} else {
+			Parents[parent1] = parent2;
 		}
+	}
+
+	private static int getParent(int child) {
+		if (Parents[child] == child)
+			return child;
+		return Parents[child] = getParent(Parents[child]);
+	}
+
+	private static void solv() throws IOException {
+		BufferedWriter write = new BufferedWriter(new OutputStreamWriter(System.out));
+		write.write((isAble() ? "YES\n" : "NO\n"));
+		write.close();
+	}
+
+	private static boolean isAble() {
+		int before = Plan.poll();
+		while (!Plan.isEmpty()) {
+			int present = Plan.poll();
+			if (!isSameParent(before, present))
+				return false;
+			before = present;
+		}
+		return true;
 	}
 
 }
