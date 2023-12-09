eb3580916f807a84e7d3bfa5e8af01f1aeb06267
gnslalsl12 <40541305+gnslalsl12@users.noreply.github.com>
2023-12-10 00:12:39 +0900

    [Gold III] Title: 택배, Time: 736 ms, Memory: 40004 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.java
+++ 백준/Gold/1719. 택배/택배.java
@@ -4,10 +4,10 @@ import java.io.*;
 public class Main {
 	static int N;
 	static int M;
-	static ArrayList<int[]>[] Ways;
-	static int[][][] Result;
+	static ArrayList<int[]>[] Ways = new ArrayList[210];
+	static int[][][] Result = new int [210][210][2];
 
-	public static void main(String args[]) throws IOException {
+	public static void main(String args[]) throws IOException { //다익스트라라
 		init();
 		solv();
 	}
@@ -17,7 +17,6 @@ public class Main {
 		StringTokenizer tokens = new StringTokenizer(read.readLine());
 		N = Integer.parseInt(tokens.nextToken());
 		M = Integer.parseInt(tokens.nextToken());
-		Ways = new ArrayList[N + 1];
 		for (int n = 1; n <= N; n++) {
 			Ways[n] = new ArrayList<>();
 		}
@@ -43,38 +42,37 @@ public class Main {
 		write.close();
 	}
 
-	private static void getMinDir() {
-		Result = new int[N + 1][N + 1][2];
+	private static void getMinDir() {   //최단 경로 구하기
 		int INF = Integer.MAX_VALUE;
 		for (int from = 1; from <= N; from++) {
 			for (int to = 1; to <= N; to++) {
 				if (from == to) {
-					Result[from][to] = new int[] { -1, -1 };
+					Result[from][to] = new int[] { -1, -1 };    //자기 자신으로의 최단 경로는 -1로 초기화
 				} else {
-					Result[from][to] = new int[] { -1, INF };
+					Result[from][to] = new int[] { -1, INF };   //나머지는 첫 경로를 -1, 경로값을 INF로 초기화
 				}
 			}
-			setDIJK(from);
+			setDIJK(from);  //from노드에서 다른 노드로의 최단 경로 정보 구하기
 		}
 	}
-
+    
 	private static void setDIJK(int from) {
 		PriorityQueue<int[]> tempPq = new PriorityQueue<>((x1, x2) -> {
 			return Integer.compare(x1[2], x2[2]);
-		}); // 도착 노드, 첫번째 경로 노드, 누적합
+		}); // {도착 노드, 첫번째 경로 노드, 경로값}
 		tempPq.add(new int[] { from, -1, 0 });
 		while (!tempPq.isEmpty()) {
 			int[] info = tempPq.poll();
 			int currentNode = info[0];
 			int firstNode = info[1];
 			int values = info[2];
-			for (int[] nextWay : Ways[currentNode]) {
+			for (int[] nextWay : Ways[currentNode]) {   //다익스트라
 				int nextNode = nextWay[0];
 				int nextValue = nextWay[1] + values;
-				if (Result[from][nextNode][1] > nextValue) {
-					Result[from][nextNode][0] = firstNode == -1 ? nextNode : firstNode;
-					Result[from][nextNode][1] = nextValue;
-					tempPq.add(new int[] { nextNode, firstNode == -1 ? nextNode : firstNode, nextValue });
+				if (Result[from][nextNode][1] > nextValue) {    //from 노드에서 nextNode로의 경로값이 최소로 갱신된다면
+					Result[from][nextNode][0] = firstNode == -1 ? nextNode : firstNode; //첫번쨰 노드번호 갱신
+					Result[from][nextNode][1] = nextValue;  //경로값 갱신
+					tempPq.add(new int[] { nextNode, firstNode == -1 ? nextNode : firstNode, nextValue });  //pq에 추가해 계속 탐색
 				}
 			}
 		}
