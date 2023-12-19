cf6d26bd770e0daeb31073a353a1ccb1e7c5cc74
gnslalsl12 <40541305+gnslalsl12@users.noreply.github.com>
2023-12-19 12:14:18 +0900

    [Silver IV] Title: 괄호, Time: 76 ms, Memory: 11832 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(read.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int test = 0; test < T; test++) {
+			String temp = read.readLine();
+			int count = 0;
+			for (int i = 0; i < temp.length(); i++) {
+				if(temp.charAt(i) == '(') count++;
+				else {
+					count--;
+					if(count == -1) {
+						sb.append("NO\n");
+						break;
+					}
+				}
+			}
+			if(count != -1) {
+				if(count == 0) sb.append("YES\n");
+				else sb.append("NO\n");
+			}
+		}
+		System.out.print(sb);
+	}
+}
